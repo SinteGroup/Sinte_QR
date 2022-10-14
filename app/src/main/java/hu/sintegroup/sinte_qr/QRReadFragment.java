5e7cb7a7307f5ca6c46fb4cbc9d5fe828d5fdb03
SinteGroup <digitalisfelmeres@gmail.com>
2022-10-14 15:39:49 +0200

    2022_10_14_Mentes

--- app/src/main/java/hu/sintegroup/sinte_qr/QRReadFragment.java
+++ app/src/main/java/hu/sintegroup/sinte_qr/QRReadFragment.java
@@ -5,11 +5,9 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Camera;
 import android.graphics.ImageFormat;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
@@ -34,23 +32,9 @@ import androidx.fragment.app.Fragment;
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
-import com.google.common.primitives.Bytes;
-import com.google.zxing.BinaryBitmap;
-import com.google.zxing.ChecksumException;
-import com.google.zxing.FormatException;
-import com.google.zxing.LuminanceSource;
-import com.google.zxing.MultiFormatReader;
-import com.google.zxing.NotFoundException;
-import com.google.zxing.RGBLuminanceSource;
-import com.google.zxing.Reader;
-import com.google.zxing.Result;
-import com.google.zxing.common.HybridBinarizer;
 
-import java.nio.Buffer;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.concurrent.Executor;
 
 import hu.sintegroup.sinte_qr.databinding.FragmentQRReadBinding;
@@ -114,15 +98,10 @@ public class QRReadFragment extends Fragment {
 
     public void onStart() {
         super.onStart();
-        /*if (ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            Log.d("CamMPermissionExp", "Nincs camera2 engedély");
-            return;
-        } else {
-            //startCameraPreview();
-        }*/
     }
 
     private void startCameraPreview(int width, int height) {
+
         try {
             Handler cameraBackgroundHandler = new Handler();
             CameraManager cameraManager = (CameraManager) getActivity().getSystemService(Context.CAMERA_SERVICE);
@@ -131,22 +110,68 @@ public class QRReadFragment extends Fragment {
                 @Override
                 public void onOpened(@NonNull CameraDevice cameraDevice) {
                     //A barcodeScenner dob hibát fordításkor
-                    BarcodeDetector barcodeDetect=new BarcodeDetector.Builder().setBarcodeFormats(Barcode.QR_CODE).build();
+                    BarcodeDetector barcodeDetect=new BarcodeDetector.Builder(getContext()).setBarcodeFormats(Barcode.QR_CODE).build();
 
-                    if(!barcodeDetect.isOperational()){
+                    if(barcodeDetect.isOperational()){
                         Log.d("Barcode", "Byrcode mukodik");
                     }
 
-                    ImageReader imageReader=ImageReader.newInstance(width, height, ImageFormat.JPEG, 1);
+                    QR_image_read=ImageReader.newInstance(width, height, ImageFormat.JPEG, 1);
+
+                    QR_image_read.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+                        @Override
+                        public void onImageAvailable(ImageReader imageReader) {
+                            Image qrImage=imageReader.acquireNextImage();
+
+                            Log.d("BarcodeQrimage", qrImage.toString());
+
+                            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                                ByteBuffer buffer= Arrays.stream(qrImage.getPlanes()).findFirst().get().getBuffer();
+                                byte[] bytes=new byte[buffer.remaining()];
+                                buffer.get(bytes);
+
+                                Bitmap qrBitmap=BitmapFactory.decodeByteArray(bytes,0, bytes.length);
+                                Frame builder=new Frame.Builder().setBitmap(qrBitmap).build();
+                                SparseArray<Barcode> barcodeResult=barcodeDetect.detect(builder);
+
+                                Log.d("BarcodeMegy", String.valueOf(barcodeResult.size()));
+
+                                if(barcodeResult.size() > 0){
+                                    Log.d("BarcodeResult", String.valueOf(barcodeResult.get(1)));
+                                }
+                            }
+                                qrImage.close();
+
+                        }
+
+                    }, cameraBackgroundHandler);
 
 
                     sessionCallBack = new CameraCaptureSession.StateCallback() {
                         @Override
                         public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
                             try {
-                                CaptureRequest.Builder builder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-                                builder.addTarget(binding.QRREadSurface.getHolder().getSurface());
-                                cameraCaptureSession.setRepeatingRequest(builder.build(), null, null);
+                                CaptureRequest.Builder Builder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                                Builder.addTarget(binding.QRREadSurface.getHolder().getSurface());
+                                Builder.addTarget(QR_image_read.getSurface());
+                                CaptureRequest request=Builder.build();
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                                    cameraCaptureSession.setSingleRepeatingRequest(request, new Executor() {
+                                        @Override
+                                        public void execute(Runnable runnable) {
+                                            Log.d("Exekjútor", "Az Exekjútor exekjútol");
+
+
+                                        }
+                                    }, new CameraCaptureSession.CaptureCallback() {
+                                        @Override
+                                        public void onCaptureStarted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, long timestamp, long frameNumber) {
+                                            super.onCaptureStarted(session, request, timestamp, frameNumber);
+
+
+                                        }
+                                    });
+                                }
                             } catch (CameraAccessException e) {
                                 Log.d("CaptureSession", e.getMessage());
                             }
@@ -158,7 +183,7 @@ public class QRReadFragment extends Fragment {
                         }
                     };
                     try {
-                        cameraDevice.createCaptureSession(Arrays.asList(binding.QRREadSurface.getHolder().getSurface()), sessionCallBack, cameraBackgroundHandler);
+                        cameraDevice.createCaptureSession(Arrays.asList(binding.QRREadSurface.getHolder().getSurface(), QR_image_read.getSurface()), sessionCallBack, cameraBackgroundHandler);
                     } catch (CameraAccessException e) {
                         Log.d("CapTureSessionEx", e.getMessage());
                     }
