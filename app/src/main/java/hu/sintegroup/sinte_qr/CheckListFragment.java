517d32516418e83b64e50dc63f23364c501c9027
SinteGroup <digitalisfelmeres@gmail.com>
2022-11-03 14:40:36 +0100

    2022_11_03_Mentés_Adatbázis_műveletek_mennek_szépen. Adatlapokat befejezni. Adatbázis titkosítást megnézni. Tistkosított kapcoslatot kiépíteni a szerverrel.

--- app/src/main/java/hu/sintegroup/sinte_qr/CheckListFragment.java
+++ app/src/main/java/hu/sintegroup/sinte_qr/CheckListFragment.java
@@ -12,7 +12,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -51,25 +53,26 @@ public class CheckListFragment extends Fragment {
         ListView checkListView=(ListView) view.findViewById(R.id.checkLista);
         ArrayList<Javitasok.Check> checkLista=new ArrayList<>();
 
+        EditText tempEditText=(EditText)view.findViewById(R.id.addColumnEditText);
+
         final String[] responseString = {""};
         RequestQueue queue = Volley.newRequestQueue(getContext());
-        String url = "https://www.weblapp.hu/Proba.php";
+        String url = "https://www.weblapp.hu/Proba.php?method=check";
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        Log.d("Volley", "Response is: " + response.contains(filterTag));
                         String[] tempString=response.split("_end_");
-                        for (String temp : tempString){
-                            if(temp.contains(filterTag)){
+                        String[] tempString2=tempString[0].split("<>");
+                        Log.d("Volley", "Response is: " + response);
+                        for (String temp : tempString2){
+                            if(/*temp.contains(filterTag)*/true){
                                 Log.d("URLVolley", temp);    //:: kulcs és értéket elválasztó szeparátor
-                                responseString[0] =temp;            // <> kulcs-érték párokat elválasztó szeparátor
-                                                                    //Iderakni az összes módosítót :(
-
-                                checkLista.add(new Javitasok.Check("Mukodik"));
-                                checkLista.add(new Javitasok.Check("Mukodik"));
-                                checkLista.add(new Javitasok.Check("Mukodik"));
+                                //responseString[0] =temp;            // <> kulcs-érték párokat elválasztó szeparátor
+                                if(!temp.contains("Datum")) {
+                                    checkLista.add(new Javitasok.Check(temp));
+                                }
 
                                 ArrayAdapter<Javitasok.Check> checkListViewAdapter=new checkListViewAdapter(checkLista, getContext());
                                 final Integer[] i = {0};
@@ -77,14 +80,33 @@ public class CheckListFragment extends Fragment {
                                 checkAddItemButton.setOnClickListener(new View.OnClickListener() {
                                     @Override
                                     public void onClick(View view) {
-                                        i[0] +=1;
-                                        Javitasok.Check tempCheck=new Javitasok.Check("Elem "+i[0]);
-                                        checkLista.add(tempCheck);
+
+                                        if(!String.valueOf(tempEditText.getText()).isEmpty()) {
+                                            Javitasok.Check tempCheck = new Javitasok.Check(String.valueOf(tempEditText.getText()));
+                                            checkLista.add(tempCheck);
+
+                                            String customColumnUrl="https://www.weblapp.hu/Proba.php?method=insertcheckcolumn&column_name="+String.valueOf(tempEditText.getText());
+                                            Log.d("customColumnUrl", customColumnUrl);
+                                            StringRequest customColumnStringRequest=new StringRequest(Request.Method.GET, customColumnUrl, new Response.Listener<String>() {
+                                                @Override
+                                                public void onResponse(String response) {
+                                                    Log.d("beagyazottResponseOk", response);
+                                                }
+                                            }, new  Response.ErrorListener(){
+
+                                                public void onErrorResponse(VolleyError error) {
+                                                    Log.e("beagyazottResponseErr", error.getMessage());
+                                                }
+                                            });
+                                            queue.add(customColumnStringRequest);
+
+                                        }else {
+                                            Toast.makeText(getContext(), "Üres a név mező.", Toast.LENGTH_LONG).show();
+                                        }
                                         checkListViewAdapter.notifyDataSetChanged();
                                     }
                                 });
                                 checkListView.setAdapter(checkListViewAdapter);
-
                             }
                         }
                     }
--- app/src/main/java/hu/sintegroup/sinte_qr/checkListViewAdapter.java
+++ app/src/main/java/hu/sintegroup/sinte_qr/checkListViewAdapter.java
@@ -1,14 +1,26 @@
 package hu.sintegroup.sinte_qr;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.util.ArrayList;
 
@@ -30,11 +42,26 @@ public class checkListViewAdapter extends ArrayAdapter<Javitasok.Check> {
             TextView elemCime=(TextView) convertView.findViewById(R.id.checkListItemId);
             elemCime.setText(tempCheck.checkName);
 
+            RequestQueue queue = Volley.newRequestQueue(getContext());
+
             Button checkItemOkButton=(Button) convertView.findViewById(R.id.checkOkButton);
             checkItemOkButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Log.d("OkButton", "OK: "+position);
+                    String CheckOkButtonUrl="https://www.weblapp.hu/Proba.php?method=insert&column="+String.valueOf(elemCime.getText())+"&values=Ok";
+                    StringRequest CheckOkButtonRequest=new StringRequest(Request.Method.GET, CheckOkButtonUrl, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.d("CheckOkNyugta", response);
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.d("CheckOkNyugtaErr", error.getMessage());
+                        }
+                    });
+                    queue.add(CheckOkButtonRequest);
                 }
             });
 
@@ -42,10 +69,42 @@ public class checkListViewAdapter extends ArrayAdapter<Javitasok.Check> {
             checkItemRepairButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Log.d("OkButton", "Repair: "+position);
+                    Log.d("RepairButton", "Repair: "+position);
+
+                    View popUpViiew=inflater.inflate(R.layout.repair_popup_window, null);
+
+                    int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                    int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                    Boolean focusable=true;
+
+                    PopupWindow repairMessagePopUp=new PopupWindow(popUpViiew, width, height, focusable);
+                    repairMessagePopUp.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+                    EditText repairMessage=(EditText)popUpViiew.findViewById(R.id.getRepairMessageEditText);
+                    RequestQueue queueRepair=Volley.newRequestQueue(getContext());
+
+                    Button repairSendMessageButton=(Button)popUpViiew.findViewById(R.id.repairWindowOkButton);
+                    repairSendMessageButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            String CheckRepairButtonUrl="https://www.weblapp.hu/Proba.php?method=insert&column="+elemCime.getText()+"&values="+repairMessage.getText();
+                            Log.d("CheckRepairButtonUrl", CheckRepairButtonUrl);
+                            StringRequest repairMessageRequest=new StringRequest(Request.Method.GET, CheckRepairButtonUrl, new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    Log.d("repair", response);
+                                }
+                            }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Log.d("repairErr", error.getMessage());
+                                }
+                            });
+                            queueRepair.add(repairMessageRequest);
+                        }
+                    });
                 }
             });
-
         return convertView;
     }
 
