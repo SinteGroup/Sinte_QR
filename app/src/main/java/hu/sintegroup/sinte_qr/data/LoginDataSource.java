640fbe73c1f7e2b78bafa1ba0e2b5213972e5d14
SinteGroup <digitalisfelmeres@gmail.com>
2022-11-25 15:51:46 +0100

    2022_11_25 Mentés. Felhasználó azonosítás megtörtént. Majd a megfelelő jogosultságkiosztást is meg kel oldani.

---
+++ app/src/main/java/hu/sintegroup/sinte_qr/data/LoginDataSource.java
@@ -0,0 +1,41 @@
+package hu.sintegroup.sinte_qr.data;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import hu.sintegroup.sinte_qr.data.model.LoggedInUser;
+import hu.sintegroup.sinte_qr.ui.login.LoginFragment;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TODO: handle loggedInUser authentication
+
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
---
+++ app/src/main/java/hu/sintegroup/sinte_qr/ui/login/LoggedInUserView.java
@@ -0,0 +1,17 @@
+package hu.sintegroup.sinte_qr.ui.login;
+
+/**
+ * Class exposing authenticated user details to the UI.
+ */
+class LoggedInUserView {
+    private String displayName;
+    //... other data fields that may be accessible to the UI
+
+    LoggedInUserView(String displayName) {
+        this.displayName = displayName;
+    }
+
+    String getDisplayName() {
+        return displayName;
+    }
+}
