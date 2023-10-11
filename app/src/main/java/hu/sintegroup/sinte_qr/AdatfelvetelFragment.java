de05679da5acf343a55781c91f5dc618e858c2f3
SinteGroup <digitalisfelmeres@gmail.com>
2023-10-11 16:01:29 +0200

    2023_10_10

--- app/src/main/java/hu/sintegroup/sinte_qr/AdatfelvetelFragment.java
+++ app/src/main/java/hu/sintegroup/sinte_qr/AdatfelvetelFragment.java
@@ -1,21 +1,15 @@
 package hu.sintegroup.sinte_qr;
 
-import android.app.DownloadManager;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
-import android.util.ArraySet;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
--- app/src/main/java/hu/sintegroup/sinte_qr/SinteSqlDatabaseHelper.java
+++ app/src/main/java/hu/sintegroup/sinte_qr/SinteSqlDatabaseHelper.java
@@ -1,16 +1,6 @@
 package hu.sintegroup.sinte_qr;
 
 import android.content.Context;
-import android.content.Intent;
-import android.os.AsyncTask;
-import android.util.Log;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
 import java.net.MalformedURLException;
 
